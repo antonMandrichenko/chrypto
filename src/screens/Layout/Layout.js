e222bac516152878e8e97218dba0187f5c129797
Anton Mandrichenko <arnellomutty2@gmail.com>
2019-10-15 15:16:18 +0300

    Load layout if no register to auth

--- src/screens/Layout/Layout.js
+++ src/screens/Layout/Layout.js
@@ -1,12 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { compose } from "redux";
+import { mapStateToProps } from "./redux";
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/react-hooks";
+
+const GET_ME = gql`
+  query GetMe($id: ID!, $username: String!, $affiliateCode: String!) {
+    getMe(id: $id, username: $username, affiliateCode: $affiliateCode) {
+      id
+      username
+      afflitiateCode
+    }
+  }
+`;
 
 const propTypes = {};
 
 function Layout(props) {
-  return <div>Layout</div>;
+  const { id } = props;
+  const [userId, setUserId] = useState("");
+  const idUser = localStorage.getItem("cryptoId");
+  const username = localStorage.getItem("cryptoUsername");
+  const affiliateCode = localStorage.getItem("cryptoAffiliateCode");
+  console.log(id, username, affiliateCode);
+
+  let { loading, error, data } = useQuery(GET_ME, {
+    variables: { id: idUser, username, affiliateCode }
+  });
+  if (data) {
+    setUserId(data.id);
+  }
+
+  if (loading) return null;
+
+  if (!userId || error) {
+    props.history.push("/auth");
+  }
+  if (id) {
+    return <div>`You are register in crypto. Yor're id is ${id}`</div>;
+  }
+
+  return <div>You are in Crypto</div>;
 }
 
 Layout.propTypes = propTypes;
 
-export default Layout;
+export default compose(
+  withRouter,
+  connect(mapStateToProps)
+)(Layout);
---
+++ src/screens/Layout/redux.js
@@ -0,0 +1,7 @@
+import { userActions } from "../../store/actions/auth.actions";
+
+export const mapStateToProps = state => {
+  return {
+    id: state.registration.id || state.authentication.id,
+  };
+};
